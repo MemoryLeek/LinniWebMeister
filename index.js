03f614cdeb39b31172678046e7b8bf742223f277
Carl Öst Wilkens <carl@ostwilkens.se>
2019-06-24 22:30:43 +0200

    Persistent votekey storage

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ var app = new Vue({
         votekey: '',
         votekeyIsValid: false,
     },
+    mounted() {
+        if(localStorage.votekey) {
+            this.votekey = localStorage.votekey;
+            this.signIn();
+        }
+    },
+    methods: {
+        signIn: function() {
+            this.votekeyIsValid = true;
+            localStorage.votekey = this.votekey;
+        },
+        signOut: function() {
+            this.votekeyIsValid = false;
+            localStorage.votekey = '';
+            this.votekey = '';
+        }
+    },
 });
